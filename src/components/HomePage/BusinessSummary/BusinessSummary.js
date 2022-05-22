8d1582b8b513ad756094e4b62c09d4c6c1e39413
eshakkhan29 <eshakkhan29@gmail.com>
2022-05-22 10:43:56 +0600

    makes home page components

---
+++ src/components/HomePage/BusinessSummary/BusinessSummary.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BusinessSummary = () => {
+    return (
+        <div>
+            <p>Business Summary</p>
+        </div>
+    );
+};
+
+export default BusinessSummary;
