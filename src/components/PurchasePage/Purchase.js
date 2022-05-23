1bebe8752e5a5176c3c2a2e7e9048ed49f3da501
eshakkhan29 <eshakkhan29@gmail.com>
2022-05-23 13:35:22 +0600

    added order plaese

--- src/components/PurchasePage/Purchase.js
+++ src/components/PurchasePage/Purchase.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import auth from '../../firebase.init';
 
 const Purchase = () => {
@@ -11,6 +12,7 @@ const Purchase = () => {
     const [error, setError] = useState('');
     const { id } = useParams();
     const [purchaseProduct, setPurchaseProduct] = useState([]);
+    const { displayName, email } = user;
     const { name, minimumOrder, quantity, price } = purchaseProduct;
 
 
@@ -37,21 +39,45 @@ const Purchase = () => {
         fetch(`http://localhost:5000/purchase/${id}`,)
             .then(res => res.json())
             .then(data => setPurchaseProduct(data))
-    }, [id])
+    }, [id]);
+
+    const handelSubmit = event => {
+        event.preventDefault();
+        const quantity = event.target.quantity.value;
+        const parts = event.target.name.value;
+        const price = event.target.price.value;
+        const address = event.target.address.value;
+        const mobile = event.target.number.value;
+        const orderData = { displayName, email, quantity, parts, price, address, mobile, };
+
+        fetch("http://localhost:5000/orders", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(orderData)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.acknowledged === true) {
+                    toast.success('your order successfully added')
+                }
+                console.log(data)
+            })
+            event.target.reset();
+
+    }
 
 
     return (
         <section className='container my-5'>
-            {
-                error && <p className='text-danger text-center fs-4'>{error}</p>
-            }
             <div className='d-flex justify-content-between'>
                 <div className='w-100'>
                     <button onClick={() => setShow(true)} className='btn btn-primary my-3'>Show Details</button>
                     {show &&
                         <>
-                            <h4>name: {user?.displayName}</h4>
-                            <h3>Email: {user?.email}</h3>
+                            <h4>name: {displayName}</h4>
+                            <h3>Email: {email}</h3>
                         </>
                     }
                     <div>
@@ -62,32 +88,35 @@ const Purchase = () => {
 
                 </div>
                 <div className='w-100'>
-                    <Form>
+                    <Form onSubmit={handelSubmit}>
                         <Form.Group className="mb-3">
                             <Form.Label>Quantity</Form.Label>
                             <Form.Control onChange={handelQuantity} name='quantity' type="text" placeholder="Quantity" />
+                            {
+                                error && <p className='text-danger'>{error}</p>
+                            }
                         </Form.Group>
                         <Form.Group className="mb-3">
                             <Form.Label>Parts name</Form.Label>
-                            <Form.Control value={name} type="text" />
+                            <Form.Control readOnly name='name' value={name} type="text" />
                         </Form.Group>
                         <Form.Group className="mb-3">
                             <Form.Label>Total price</Form.Label>
-                            <Form.Control value={totalPrice} type="text" />
+                            <Form.Control readOnly name='price' value={totalPrice} type="text" />
                         </Form.Group>
 
                         <Form.Group className="mb-3">
                             <Form.Label>Address</Form.Label>
-                            <Form.Control type="number" placeholder="your Address" />
+                            <Form.Control required name='address' type="text" placeholder="your Address" />
                         </Form.Group>
 
                         <Form.Group className="mb-3">
                             <Form.Label>Phone</Form.Label>
-                            <Form.Control type="number" placeholder="Phone number" />
+                            <Form.Control required name='number' type="number" placeholder="Phone number" />
                         </Form.Group>
 
                         <Button variant="primary" value='Order' type="submit">
-                            Submit
+                            SET ORDER
                         </Button>
                     </Form>
                 </div>
